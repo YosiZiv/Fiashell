65b4218ed13487efeaee43592cdd71ca54321089
katlany <notan8@gmail.com>
2020-04-07 05:31:48 +0300

    add toggle local state

--- client/src/components/pages/main/Main.js
+++ client/src/components/pages/main/Main.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import SlideIn from "../../layouts/SlideIn/SlideIn";
+import Register from "../register/Register";
 import "./Main.scss";
 const Main = () => {
+  const [slideToggle, setSlideToggle] = useState(false);
+  const slideInToggle = () => {
+    setSlideToggle(!slideToggle);
+  };
   return (
     <div className='main-container'>
-      <div className='MainPageContent'>
+      <div className='main-header'>
         <h2>MPPUM</h2>
         <p>My Pivate Product User Managment </p>
-        <button className='btn btn-primary' type='button'>
+        <button onClick={slideInToggle} type='button'>
           Become A Manager
         </button>
+      </div>
+      <div className='main-register'>
+        <SlideIn open={slideToggle}>
+          <Register />
+        </SlideIn>
       </div>
     </div>
   );
