d1d1550c3b7afdaa737fd12075502ebc3feee56c
katlany <notan8@gmail.com>
2020-04-12 11:05:27 +0300

    improve regisrer page component

--- client/src/components/pages/register/register.js
+++ client/src/components/pages/register/register.js
@@ -3,110 +3,107 @@ import { connect } from "react-redux";
 import Input from "../../layouts/TextInput/TextInput";
 import { inputChange } from "../../../redux/actions/register";
 import "./Register.scss";
-const Register = (props) => {
-  const { loading, registerForm } = props;
-  const handleInputChange = ({ id, value, validation }) => {
-    const { inputChange } = props;
+const Register = ({ loading, registerForm, inputChange }) => {
+  const handleInputChange = (event, id, validation) => {
+    event.preventDefault();
+    const { value } = event.currentTarget;
     inputChange({ id, value, validation });
   };
   return (
-    <>
+    <div className='register-container'>
       <div className='register-title'>
         <h2> BE ADMIN</h2>
       </div>
+      <div className='register-form'>
+        <div onSubmit={(e) => e.preventDefault()}>
+          <Input
+            id='firstName'
+            name='First Name'
+            type='text'
+            required
+            validation={{
+              isRequired: true,
+              minLength: 2,
+              maxLength: 256,
+            }}
+            error={registerForm.firstName?.error}
+            disabled={loading}
+            value={registerForm.firstName?.value ?? ""}
+            handleInputChange={handleInputChange}
+          />
 
-      <form className='register-form' onSubmit={(e) => e.preventDefault()}>
-        <Input
-          isValid={registerForm.firstName?.isValid ?? true}
-          className='form-control'
-          id='firstName'
-          name='FIRST NAME'
-          type='text'
-          required
-          disabled={loading}
-          defaultValue={registerForm.firstName?.value}
-          inputChange={handleInputChange}
-          validation={{
-            isRequired: true,
-            minLength: 2,
-            maxLength: 256,
-          }}
-        />
-        <Input
-          isValid={registerForm.lastName?.isValid ?? true}
-          className='form-control'
-          id='lastName'
-          name='LAST NAME'
-          type='text'
-          required
-          disabled={loading}
-          defaultValue={registerForm.lastName?.value}
-          inputChange={handleInputChange}
-          validation={{
-            isRequired: true,
-            minLength: 2,
-            maxLength: 256,
-          }}
-        />
+          <Input
+            id='lastName'
+            name='Last Name'
+            type='text'
+            required
+            validation={{
+              isRequired: true,
+              minLength: 2,
+              maxLength: 256,
+            }}
+            error={registerForm.lastName?.error}
+            disabled={loading}
+            value={registerForm.lastName?.value ?? ""}
+            handleInputChange={handleInputChange}
+          />
+          <Input
+            id='phone'
+            name='Phone'
+            type='text'
+            required
+            validation={{
+              isRequired: true,
+              minLength: 6,
+              maxLength: 15,
+            }}
+            error={registerForm.phone?.error}
+            disabled={loading}
+            value={registerForm.phone?.value ?? ""}
+            handleInputChange={handleInputChange}
+          />
 
-        <Input
-          isValid={registerForm.phone?.isValid ?? true}
-          className='form-control'
-          id='phone'
-          name='PHONE'
-          type='text'
-          required
-          disabled={loading}
-          defaultValue={registerForm.phone?.value}
-          inputChange={handleInputChange}
-          validation={{
-            isRequired: true,
-            minLength: 6,
-            maxLength: 256,
-          }}
-        />
-        <Input
-          isValid={registerForm.email?.isValid ?? true}
-          className='form-control'
-          id='email'
-          name='EMAIL'
-          type='email'
-          required
-          disabled={loading}
-          defaultValue={registerForm.email?.value}
-          inputChange={handleInputChange}
-          validation={{ isRequired: true, isEmail: true }}
-        />
+          <Input
+            id='email'
+            name='Email'
+            type='email'
+            required
+            validation={{ isRequired: true, isEmail: true }}
+            error={registerForm.email?.error}
+            disabled={loading}
+            value={registerForm.email?.value ?? ""}
+            handleInputChange={handleInputChange}
+          />
 
-        <Input
-          isValid={registerForm.password?.isValid ?? true}
-          className='form-control'
-          id='password'
-          name='PASSWORD'
-          type='password'
-          required
-          disabled={loading}
-          defaultValue={registerForm.password?.value}
-          inputChange={handleInputChange}
-          validation={{ isRequired: true, minLength: 6 }}
-        />
-        <Input
-          isValid={registerForm.password?.isValid ?? true}
-          className='form-control'
-          id='passwordConfirm'
-          name='RE PASSWORD'
-          type='password'
-          required
-          disabled={loading}
-          defaultValue={registerForm.passwordConfirm?.value}
-          inputChange={handleInputChange}
-          validation={{ isRequired: true, minLength: 6 }}
-        />
-        <div className='registerSubmit'>
-          <button className='btn btn-primary'>BE ADMIN</button>
+          <Input
+            id='password'
+            name='Password'
+            type='password'
+            required
+            validation={{ isRequired: true, minLength: 6 }}
+            error={registerForm.password?.error}
+            disabled={loading}
+            value={registerForm.password?.value ?? ""}
+            handleInputChange={handleInputChange}
+          />
+
+          <Input
+            id='passwordConfirm'
+            name='Password Confirm'
+            type='password'
+            required
+            validation={{ isRequired: true, minLength: 6 }}
+            error={registerForm.passwordConfirm?.error}
+            disabled={loading}
+            value={registerForm.passwordConfirm?.value ?? ""}
+            handleInputChange={handleInputChange}
+          />
         </div>
-      </form>
-    </>
+      </div>
+      <div className='registerSubmit'>
+        <button className='btn btn-primary'>BE ADMIN</button>
+      </div>
+    </div>
   );
 };
 const mapStateToProps = (state) => {
