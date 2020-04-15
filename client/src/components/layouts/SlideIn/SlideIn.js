a36259663df79f195a45c4e71e3c08dde7c0bdf7
katlany <notan8@gmail.com>
2020-04-15 11:30:50 +0300

    props bug fix

--- client/src/components/layouts/SlideIn/SlideIn.js
+++ client/src/components/layouts/SlideIn/SlideIn.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./SlideIn.scss";
 const slideIn = ({ open = false, close = false, children }) => {
-  const { open, children } = props;
   //TODO ADD CLOSE METHOD
   const openCheck = open ? " is-open" : "";
   return <div className={"slider" + openCheck}>{children}</div>;
