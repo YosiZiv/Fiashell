3f24b1e52c7c505f09e00fa249297ace3e97f6b9
katlany <notan8@gmail.com>
2020-04-15 11:20:10 +0300

    add login component for main page

--- client/src/components/pages/main/Main.js
+++ client/src/components/pages/main/Main.js
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
 import SlideIn from "../../layouts/SlideIn/SlideIn";
 import Register from "../register/register";
-import { NavLink } from "react-router-dom";
+import Login from "../login/Login";
 import "./Main.scss";
+
 const Main = () => {
-  const [slideToggle, setSlideToggle] = useState(false);
-  const slideInToggle = () => {
-    setSlideToggle(!slideToggle);
+  const [slideToggle, setSlideToggle] = useState(null);
+  const slideInOpen = (e) => {
+    console.log(e.currentTarget.value);
+    setSlideToggle(e.currentTarget.id);
   };
+  console.log(slideToggle);
+
   return (
     <div className='main-container'>
       <div className='main-header'>
         <h2>MPPUM</h2>
         <p>My Pivate Product User BULBULnt </p>
-        <button onClick={slideInToggle} type='button'>
+        <button id='register' onClick={slideInOpen} type='button'>
           Register
         </button>
-        <button type='button'>
-          <NavLink activeClassName='NavSelect' to='/login' className='navLink'>
-            Login
-          </NavLink>
+        <button id='login' type='button' onClick={slideInOpen}>
+          Login
         </button>
       </div>
       <SlideIn open={slideToggle}>
-        <Register />
+        {(slideToggle === "register" && <Register />) ||
+          (slideToggle === "login" && <Login />)}
       </SlideIn>
     </div>
   );
