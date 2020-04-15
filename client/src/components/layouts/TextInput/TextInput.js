7760a4aa75e3abcf33013f4df10fea1b8a4f3849
katlany <notan8@gmail.com>
2020-04-15 14:00:46 +0300

    improve css input

--- client/src/components/layouts/TextInput/TextInput.js
+++ client/src/components/layouts/TextInput/TextInput.js
@@ -10,28 +10,32 @@ const textInput = ({
   disabled,
   value,
   handleInputChange,
-}) => (
-  <div className='text-input-container'>
-    <h6>
-      <strong>
-        {name} {required && <span className='text-input-header'>*</span>}
-      </strong>
-    </h6>
+}) => {
+  const inputClass = error ? "text-input invalid" : "text-input";
+  console.log(inputClass);
 
-    <input
-      id={id}
-      type={type}
-      value={value}
-      disabled={disabled}
-      onChange={(e) => handleInputChange(e, id, validation)}
-      placeholder={`Enter ${name}`}
-      autoComplete='off'
-      className={error && "invalid"}
-    />
-    <div className='text-input-error-message'>
-      {error && <small className='text-danger'>{error}</small>}
+  return (
+    <div className='text-input-container'>
+      <p>
+        <strong>
+          {name} {required && <span className='text-input-header'>*</span>}
+        </strong>
+      </p>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        disabled={disabled}
+        onChange={(e) => handleInputChange(e, id, validation)}
+        placeholder={`Enter ${name}`}
+        autoComplete='off'
+        className={inputClass}
+      />
+      <div className='text-input-error-message'>
+        {error && <small className='text-danger'>{error}</small>}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default textInput;
