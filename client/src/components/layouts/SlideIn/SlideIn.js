7ccc7b130a806343e9bb4dae79e88ee5e7c0c1f0
katlany <notan8@gmail.com>
2020-05-01 14:55:25 +0300

    add close model

--- client/src/components/layouts/SlideIn/SlideIn.js
+++ client/src/components/layouts/SlideIn/SlideIn.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./SlideIn.scss";
 const slideIn = ({ open = false, close = false, children }) => {
   //TODO ADD CLOSE METHOD
-  const openCheck = open ? " is-open" : "";
+  const openCheck = open ? " is-open" : " is-close";
   return <div className={"slider" + openCheck}>{children}</div>;
 };
 
