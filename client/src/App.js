ddad2255c547d9adb64c88d527e1e2780c94ce9b
katlany <notan8@gmail.com>
2020-05-04 22:17:55 +0300

    create navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Main from "./components/pages/main/Main";
+import Register from "./components/pages/main/Main";
+import Login from "./components/pages/main/Main";
+import Navigation from "./components/layouts/Navigation/Navigation";
 import "./App.scss";
 
 function App() {
@@ -8,11 +11,18 @@ function App() {
     <>
       <Switch>
         <Route path='/' component={Main} exact />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
       </Switch>
     </>
   );
 
-  return <div className='App'>{routes}</div>;
+  return (
+    <div className='App'>
+      <Navigation isAuth={true} />
+      {routes}
+    </div>
+  );
 }
 
 export default App;
